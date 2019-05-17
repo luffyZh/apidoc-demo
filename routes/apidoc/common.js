bc8e97e145d4e99de2dac3b5b694c1282ff05ab0
luffyZhou <zhoudeyou945@126.com>
2019-05-17 14:37:57 +0800

    pull away common-api-block

--- routes/apidoc/common.js
+++ routes/apidoc/common.js
@@ -1,3 +1,4 @@
+// 抽离公共代码块
 /**
  * @apiDefine CommonSuccess 公共响应字段
  * @apiSuccess {Number} errcode The success res code.
@@ -8,6 +9,11 @@
   * @apiDefine token 需要验证用户权限
   * 需要在header中加入Authorization字段进行用户权限验证 
   */
+
+/**
+ * @apiDefine none 不需要任何权限
+ * permission定义成none，不需要任何权限就可访问接口
+ */
 
 /**
  * @apiDefine InvalidToken 权限认证失败
